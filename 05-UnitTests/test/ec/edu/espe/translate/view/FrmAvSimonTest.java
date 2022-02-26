ba27892918a81ffd701e5f1012cf39f7c667d789
jrimbaquingaguana <ricardoimbaquinga@gmail.com>
2022-02-26 08:00:16 -0500

    05-UnitTests-V1.2

---
+++ 05-UnitTests/test/ec/edu/espe/translate/view/FrmAvSimonTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.translate.view;
+
+import ec.edu.espe.translate.view.FrmAvSimon;
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JoseRichard
+ */
+public class FrmAvSimonTest {
+    
+    public FrmAvSimonTest() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of main method, of class FrmAvSimon.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        FrmAvSimon.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
---
+++ 05-UnitTests/test/ec/edu/espe/translate/view/FrmGoogle1Test.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.translate.view;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JoseRichard
+ */
+public class FrmGoogle1Test {
+    
+    public FrmGoogle1Test() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of transparenciaText method, of class FrmGoogle1.
+     */
+    @Test
+    public void testTransparenciaText() {
+        System.out.println("transparenciaText");
+        FrmGoogle1 instance = new FrmGoogle1();
+        instance.transparenciaText();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of transparenciatexto method, of class FrmGoogle1.
+     */
+    @Test
+    public void testTransparenciatexto() {
+        System.out.println("transparenciatexto");
+        FrmGoogle1 instance = new FrmGoogle1();
+        instance.transparenciatexto();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of transparenciaButton method, of class FrmGoogle1.
+     */
+    @Test
+    public void testTransparenciaButton() {
+        System.out.println("transparenciaButton");
+        FrmGoogle1 instance = new FrmGoogle1();
+        instance.transparenciaButton();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of main method, of class FrmGoogle1.
+     */
+    @Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        FrmGoogle1.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
